f3ac7c4b6498afb8afe86b8ceac497100eb2245a
Logan Duvick <loganduvick@gmail.com>
2024-01-24 15:34:21 -0500

    update three

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 
 def apt_search1(city,max_rent,min_beds,pets_allowed):
-
     if pets_allowed:
-        pets=" are allowed "
+        print("Welcome to CG Property Management! Looking up listings in " + city + " for " + str(
+            min_beds) + " bedroom apartments that are allowed, all within a budget of " + str(
+            max_rent) + " per month...")
+
     else:
-        pets=" are not allowed "
+        print("Welcome to CG Property Management! Looking up listings in " + city + " for " + str(
+            min_beds) + " bedroom apartments, all within a budget of " + str(max_rent) + " per month...")
 
-    print("Welcome to CG Property Management! Looking up listings in "+city+" for"+str(min_beds)+" bedroom apartments that" + pets +", all within a budget of "+str(max_rent)+" per month...")
 
-
-def apt_search2(city,max_rent,min_beds,pets_allowed):
-    min_beds = 1
-    pets_allowed = False
+def apt_search2(city,max_rent,min_beds=1,pets_allowed=False):
+    #min_beds = 1
+    #pets_allowed = False
     if pets_allowed:
-        pets=" are allowed "
+        print("Welcome to CG Property Management! Looking up listings in " +city+" for "+str(min_beds)+" bedroom apartments that are allowed, all within a budget of "+str(max_rent)+" per month...")
+
     else:
-        pets="are not allowed "
+        print(f"Welcome to CG Property Management! Looking up listings in {city} for {min_beds} bedroom apartments, all within a budget of {max_rent} per month...")
 
-    print("Welcome to CG Property Management! Looking up listings in " +city+" for"+str(min_beds)+" bedroom apartments that" + pets +", all within a budget of "+str(max_rent)+" per month...")
 
-max_rent=int(input("What is your budget for rent?: "))
-city=input("What city would you like to search for?: ")
-min_beds=int(input("How many"))
-pets_allowed=bool(input("Do you have any pets (True/False): "))
-apt_search1(city,max_rent ,min_beds,pets_allowed)
-apt_search2(city ,max_rent ,min_beds ,pets_allowed)
+
+
+apt_search1("San Francisco",1400,3, True )
+apt_search2("Charlotte",1200)
+apt_search2("San Francisco", 1100, 2)
+apt_search2(city="San Francisco", max_rent=1000, pets_allowed=True)
 
