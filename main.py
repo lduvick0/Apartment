cc6ec14aeb1a8319e8bff611be2017728f6b8a6e
Logan Duvick <loganduvick@gmail.com>
2024-01-24 14:50:49 -0500

    update one

---
+++ main.py
@@ -0,0 +1,7 @@
+city="San Francisco"
+max_rent=0
+min_beds=0
+pets_allowed=True
+
+def apt_search1(city,max_rent,min_beds,pets_allowed):
+    print("Welcome to CG Property Management! Looking up listings in "+city+" for"+str(min_beds)+" bedroom apartments that allow pets, all within a budget of "+str(max_rent)+" per month...")
